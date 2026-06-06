b02be97ec1b3cec1467447b1d192c7268c338353
luizpaulacs <luizpaulacs@gmail.com>
2026-06-06 17:00:44 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,62 +1,38 @@
 import { auth } from "./firebase.js";
 
 import {
-
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword
-
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const email = document.getElementById("email");
-
 const senha = document.getElementById("senha");
-
 const loginBtn = document.getElementById("loginBtn");
-
 const cadastroBtn = document.getElementById("cadastroBtn");
-
 const mensagem = document.getElementById("mensagem");
 
 loginBtn.addEventListener("click", async () => {
+  const e = email.value.trim();
+  const s = senha.value.trim();
 
   try {
-
-    await signInWithEmailAndPassword(
-      auth,
-      email.value,
-      senha.value
-    );
-
+    await signInWithEmailAndPassword(auth, e, s);
     window.location.href = "dashboard.html";
-
-  } catch(error){
-
-    mensagem.innerHTML = error.message;
-
-    console.log(error);
-
+  } catch (error) {
+    console.log(error.code, error.message, error.customData);
+    mensagem.textContent = `${error.code}: ${error.message}`;
   }
-
 });
 
 cadastroBtn.addEventListener("click", async () => {
+  const e = email.value.trim();
+  const s = senha.value.trim();
 
   try {
-
-    await createUserWithEmailAndPassword(
-      auth,
-      email.value,
-      senha.value
-    );
-
+    await createUserWithEmailAndPassword(auth, e, s);
     window.location.href = "dashboard.html";
-
-  } catch(error){
-
-    mensagem.innerHTML = error.message;
-
-    console.log(error);
-
+  } catch (error) {
+    console.log(error.code, error.message, error.customData);
+    mensagem.textContent = `${error.code}: ${error.message}`;
   }
-
 });
